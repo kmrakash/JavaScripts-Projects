a7dfd521f523cfa050078657434a371a9de7d3d4
Akash Kumar <aksh.iem22@gmail.com>
2020-07-02 10:08:27 +0530

    UI and Rule

---
+++ Number Guesser/app.js
@@ -0,0 +1,9 @@
+/*
+GAME FUNCTION:
+- Player must guess a number between a min and max
+- Player gets a certain amount of guesses
+- Notify player of guesses remaining
+- Notify the player of the correct answer if loose
+- Hint player the number is greater or lesser than correct answer
+- Let player choose to play again
+*/
